7a79055738691e00498bde4e54f73f1c661a8a32
idlivada <idlivada@users.noreply.github.com>
2017-06-07 11:54:42 -0400

    Add timeout

--- adblock/fullscreen/fullscreen-adblock.js
+++ adblock/fullscreen/fullscreen-adblock.js
@@ -1,7 +1,9 @@
       window.onload = function() {
-        if(typeof mktoMunchkin === 'undefined')  {
-          document.body.innerHTML = '<div style="background-color:black; height:100%;"><div style="position: absolute; top: 50%; left: 50%; transform: translateX(-50%) translateY(-50%);"><h1 style="color:white; text-align:center; font-family:arial;">Please disable your adblocking software for this site and <a href="javascript:location.reload()" style="color:white;">click here</a> to continue.</h1></div></div>';
-         document.body.setAttribute("style", "margin:0px;");
-        }
+        setTimeout(function() {
+              if(typeof mktoMunchkin === 'undefined')  {
+                document.body.innerHTML = '<div style="background-color:black; height:100%;"><div style="position: absolute; top: 50%; left: 50%; transform: translateX(-50%) translateY(-50%);"><h1 style="color:white; text-align:center; font-family:arial;">Please disable your adblocking software for this site and <a href="javascript:location.reload()" style="color:white;">click here</a> to continue.</h1></div></div>';
+               document.body.setAttribute("style", "margin:0px;");
+              }
+        }, 1000);
       }
 
